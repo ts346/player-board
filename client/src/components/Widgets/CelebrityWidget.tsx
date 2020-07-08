9269ab12744df6db9edc3205002fe779da6017c2
Leo Gharib <leonargharib@gmail.com>
2020-07-08 13:11:49 -0700

    resolve warnings

--- client/src/components/Widgets/CelebrityWidget.tsx
+++ client/src/components/Widgets/CelebrityWidget.tsx
@@ -1,31 +1,32 @@
 import React, { useEffect, useState } from "react";
-import axios from 'axios';
-import clock from './images/clock.png';
+
+import axios from "axios";
+import clock from "./images/clock.png";
 declare const window: any;
 //Celebrity Widget
 //Celebrity Image
-export const CelebrityWidget = () =>{
-    //Image states
-    const [optionValue, setOptionValue] = useState('kanye')
-    const [itemIndex, setItemIndex] = useState(0)
-    const items = [
-      { label: "kanye",value: require('./images/Kanye.png')},
-      { label: "lebron", value: require('./images/Lebron.png')},
-      { label: "the rock", value: require('./images/Rock.png')},
-    ];
-    //Time state
-    const [time, setTime] = useState("");
-    //Image functions
-    const handleChange = (evt: { target: { value: any; }; }) => {
-   const newValue = evt.target.value
-   setOptionValue(newValue)
-   const newItemIndex = items.findIndex(items => items.label === newValue)
-   setItemIndex(newItemIndex)
-  }
+export const CelebrityWidget = () => {
+  //Image states
+  const [optionValue, setOptionValue] = useState("kanye");
+  const [itemIndex, setItemIndex] = useState(0);
+  const items = [
+    { label: "kanye", value: require("./images/Kanye.png") },
+    { label: "lebron", value: require("./images/Lebron.png") },
+    { label: "the rock", value: require("./images/Rock.png") },
+  ];
+  //Time state
+  const [time, setTime] = useState("");
+  //Image functions
+  const handleChange = (evt: { target: { value: any } }) => {
+    const newValue = evt.target.value;
+    setOptionValue(newValue);
+    const newItemIndex = items.findIndex((items) => items.label === newValue);
+    setItemIndex(newItemIndex);
+  };
   //Time functions
   const getFormattedTime = () => {
     const newDate = new Date(Date.now());
-  
+
     return `${newDate.getHours()}:
     ${newDate.getMinutes().toString().padStart(2, "0")}`;
   };
@@ -33,22 +34,23 @@ export const CelebrityWidget = () =>{
   const sendAPIRequest = async () => {
     const accounts = await window.ethereum.enable();
     const account = accounts[0];
-    window.alert('A Gift For You'+'\n'+'\n'+'1 RAWR Token');
-        var apiAddress;
-        apiAddress = "https://adventure-eth-api.herokuapp.com/transfer-token";
-        axios.post(apiAddress, {
-            ticker: "BEAR",
-            amount: 1,
-            to: account,
-            hookUrl: "done",
-        })
-            .then(function (response) {
-                console.log(response);
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-  }
+    window.alert("A Gift For You\n\n1 RAWR Token");
+    var apiAddress;
+    apiAddress = "https://adventure-eth-api.herokuapp.com/transfer-token";
+    axios
+      .post(apiAddress, {
+        ticker: "BEAR",
+        amount: 1,
+        to: account,
+        hookUrl: "done",
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
   useEffect(() => {
     setInterval(() => {
       const newTime = getFormattedTime();
@@ -56,27 +58,30 @@ export const CelebrityWidget = () =>{
     }, 1000);
     setTimeout(sendAPIRequest, 60000);
   }, []);
-    return (
-      <div id="celebrity" className="default">
-        <header>
-          <img alt="clock" className="clock" src={clock} />
-          <h1>{items[itemIndex].label} time</h1>
-          <div className="dropdown">
-            <select className="dropbutton" value={optionValue} onChange={handleChange}>
-              <option value="kanye">Kanye</option>
-              <option value="lebron">Lebron</option>
-              <option value="the rock">The Rock</option>
-            </select>
-          </div>
-        </header>
-        <main>
-          <img alt="celebrity picture" className="image" src={items[itemIndex].value} />
-          <div className="time">
-              The time is <div>{time}</div>
-          </div>
-          <div>
+  return (
+    <div id="celebrity" className="default">
+      <header>
+        <img alt="clock" className="clock" src={clock} />
+        <h1>{items[itemIndex].label} time</h1>
+        <div className="dropdown">
+          <select
+            className="dropbutton"
+            value={optionValue}
+            onChange={handleChange}
+          >
+            <option value="kanye">Kanye</option>
+            <option value="lebron">Lebron</option>
+            <option value="the rock">The Rock</option>
+          </select>
         </div>
-        </main>
-      </div>
-  )
-  }
+      </header>
+      <main>
+        <img alt="celebrity" className="image" src={items[itemIndex].value} />
+        <div className="time">
+          The time is <div>{time}</div>
+        </div>
+        <div></div>
+      </main>
+    </div>
+  );
+};
